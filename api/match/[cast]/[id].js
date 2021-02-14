f3e55699363685fe3f5728d42dfa58ad093b79e7
Kévin Painchaud <mail@kpcd.me>
2021-02-14 10:54:03 +0100

    Revert "🚚 Move crawler dependency into api endpoints"
    This reverts commit d0938acaf9b80c02fe7c297c628216b6e81c9f93.

--- api/match/[cast]/[id].js
+++ api/match/[cast]/[id].js
@@ -1,4 +1,3 @@
-const Crawler = require("crawler");
 const OwlWebsiteCrawler = require("../../../lib/owl-website-crawler");
 const casts = require("../../../data/casts");
 
@@ -6,11 +5,7 @@ module.exports = async (req, res) => {
   let match = {};
 
   if (casts.includes(req.query.cast)) {
-    match = await OwlWebsiteCrawler.getMatch(
-      Crawler,
-      req.query.cast,
-      req.query.id
-    );
+    match = await OwlWebsiteCrawler.getMatch(req.query.cast, req.query.id);
   }
 
   res.setHeader("Content-Type", "application/json");
--- api/matches.js
+++ api/matches.js
@@ -1,9 +1,8 @@
-const Crawler = require("crawler");
 const OwlWebsiteCrawler = require("../lib/owl-website-crawler");
 const casts = require("../data/casts");
 
 module.exports = async (req, res) => {
-  const matches = await OwlWebsiteCrawler.getMatches(Crawler, casts, 10);
+  const matches = await OwlWebsiteCrawler.getMatches(casts, 10);
 
   res.setHeader("Content-Type", "application/json");
   res.end(JSON.stringify(matches));
