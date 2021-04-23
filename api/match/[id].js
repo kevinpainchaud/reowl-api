da20acf5930495bb8e97269fa5c7d10a4b4c6f06
Kévin Painchaud <mail@kpcd.me>
2021-04-23 07:36:42 +0200

    🔧 Update cache policy

--- api/match/[id].js
+++ api/match/[id].js
@@ -4,7 +4,7 @@ const OwlWebsiteCrawler = require("../../lib/owl-website-crawler");
 module.exports = async (req, res) => {
   let match = await OwlWebsiteCrawler.getMatch(Crawler, req.query.id);
 
-  res.setHeader("Cache-Control", "s-maxage=1, stale-while-revalidate=259200");
+  res.setHeader("Cache-Control", "max-age=0, s-maxage=259200");
   res.setHeader("Content-Type", "application/json");
   res.end(JSON.stringify(match));
 };
